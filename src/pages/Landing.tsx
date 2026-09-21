52041c8dd1e2fc528864b4ddd422ae799397f6bf
pauljrad <123997834+pauljrad@users.noreply.github.com>
2026-09-21 17:34:10 +0100

    Make landing hero cover full iPhone viewport

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -30,7 +30,7 @@ const Landing = () => {
             <main className="relative z-10 font-body">
 
                 {/* Section 1: Intro (Black) */}
-                <section className="h-[100dvh] min-h-[100dvh] w-full bg-black flex flex-col items-center justify-start pt-12">
+                <section className="h-[calc(100dvh+env(safe-area-inset-bottom,0px))] min-h-[calc(100dvh+env(safe-area-inset-bottom,0px))] w-full bg-black flex flex-col items-center justify-start pt-12">
                 </section>
 
                 <KineticScrollSection />
